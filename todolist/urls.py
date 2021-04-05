ebc4bb928f0baa6973a2f1a1f85228004c6ba359
Andres Bonilla <abonilla@eventbrite.com>
2021-04-05 16:24:06 -0300

    Add empty view for /todo path

--- todolist/urls.py
+++ todolist/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todolist_app.views import todolist_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todo/', todolist_view),
 ]
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+
+
+def todolist_view(request):
+    return HttpResponse('<h1>Asdasd</h1>')
